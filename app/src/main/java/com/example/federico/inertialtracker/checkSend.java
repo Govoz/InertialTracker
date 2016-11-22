026f55fceda16633a3ea8053d0fdd23327d3d179
Federico Govoni <govoni.federico@gmail.com>
2016-11-22 18:30:45 +0100

    Dovrei esser riuscito a fare i dovuti controlli sul Wifi. E' da testare bene e commentare.
    Manca il send

--- app/src/main/java/com/example/federico/inertialtracker/checkSend.java
+++ app/src/main/java/com/example/federico/inertialtracker/checkSend.java
@@ -1,10 +1,19 @@
 package com.example.federico.inertialtracker;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
+
+import java.util.List;
 
 /**
  * Created by Federico on 15-Nov-16.
@@ -24,10 +33,128 @@ public class checkSend extends ParallelIntentService {
 	protected void onHandleIntent(Intent intent) {
 		Log.d(TAG, "onHandleIntent(Intent); Started, thread id: " + Thread.currentThread().getId());
 		try {
+
+			// se sono connesso invio i dati
+			if(checkConnection()){
+				sendData();
+			}
+
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+
+
+	// Function return true if I am actually connected to Wifi Connection.
+	public boolean checkConnection(){
+		ConnectivityManager connectManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetwork = connectManager.getActiveNetworkInfo();
+
+		// sono connesso
+		if(activeNetwork != null){
+			if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI){
+				if(activeNetwork.isConnected()){
+					WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+					WifiInfo networkInfo = wm.getConnectionInfo();
+					if(networkInfo != null){
+						writeWifiNetworkLog(networkInfo);
+					}
+				}
+				return true;
+			}
+		}
+		else {
+			// cerco una rete a cui connettermi
+			ScanResult wifiNetwork = searchFreeWifi();
+
+			// mi connetto
+			connectToWifi(wifiNetwork);
+
+			//ricontrollo la networkActive
+			activeNetwork = connectManager.getActiveNetworkInfo();
+
+			// se sono riuscito a collegarmi ad una rete
+			if(activeNetwork != null){
+				if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI){
+					writeWifiNetworkLog(wifiNetwork);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	// provo a connettermi a wifiNetwork
+	private boolean connectToWifi(ScanResult wifiNetwork) {
+		WifiConfiguration wc = new WifiConfiguration();
+		String networkSSID = wifiNetwork.SSID;
+		wc.SSID = "\"" + networkSSID +"\"";
+
+		wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+
+		WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		int netID = wm.addNetwork(wc);
+		wm.disconnect();
+		wm.enableNetwork(netID, true);
+		return (wm.reconnect());
+	}
+
+
+	// Function that search Free Wifi Connection to connect it.
+	private ScanResult searchFreeWifi() {
+		WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+		List <ScanResult> listScanResult = wifiManager.getScanResults();
+
+		int maxPoverRSSI = 0;
+		ScanResult wifiToConnect = null;
+
+		for (ScanResult result : listScanResult){
+			String capability = result.capabilities;
+
+			// Open Network
+			if(!((capability.toUpperCase().contains("WEP")) ||
+					capability.toUpperCase().contains("WPA"))){
+
+				//Search WiFi with max RSSI
+				if(result.level > maxPoverRSSI){
+					wifiToConnect = result;
+					maxPoverRSSI = result.level;
+				}
+			}
+		}
+		return wifiToConnect;
+	}
+
+	private void writeWifiNetworkLog(ScanResult wifiNetwork) {
+		writeLogFile wlog = new writeLogFile();
+
+		String msg = "WIFI - " +
+				"TimeStamp : " + wifiNetwork.timestamp + " /n" +
+				"BSSID : " + wifiNetwork.BSSID + " /n" +
+				"SSID : " + wifiNetwork.SSID + " /n" +
+				"RSSI : " + wifiNetwork.level + " /n";
+
+		wlog.write(this, MainActivity.FILENAME, msg, Context.MODE_APPEND);
+	}
+
+	private void writeWifiNetworkLog(WifiInfo wifiNetwork) {
+		writeLogFile wlog = new writeLogFile();
+
+		String msg = "WIFI - " +
+				"TimeStamp : " + System.currentTimeMillis() + " /n" +
+				"BSSID : " + wifiNetwork.getBSSID() + " /n" +
+				"SSID : " + wifiNetwork.getSSID() + " /n" +
+				"RSSI : " + wifiNetwork.getRssi() + " /n";
+
+		wlog.write(this, MainActivity.FILENAME, msg, Context.MODE_APPEND);
+	}
+
+	public void sendData(){
+		//apro il file
+		//connetto al server
+		//invio
 	}
 
 }
